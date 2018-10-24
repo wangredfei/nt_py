2f6352dff28434dbcce6702fbe9ff0341f2fa942
xiaoAnn <289498360@qq.com>
2018-10-24 09:29:16 +0800

    iter

--- lxzy_14/lx10_iter_next.py
+++ lxzy_14/lx10_iter_next.py
@@ -1,12 +1,23 @@
 s = {'tangse','sundasheng','zhubajie','baigujing','shawujin'}
-# for i in s:
-#     print(i)
-# else :
-#     print("jieshu")
+for i in s:
+    print(i)
+else :
+    print("jieshu")
 # people = iter(s)
-while 1 :
-    try:
-        print(next(iter(s)))
-    except StopIteration:
-        print("遍历结束")
-        break
+# 注意
+# while 1 :
+#     try:
+#         print(next(iter(s)))
+#     except StopIteration:
+#         print("遍历结束")
+#         break
+
+
+迭代器
+it  = iter("123123")
+print(it)
+print(next(it))
+print(next(it))
+print("-"*50)
+for i in it :
+    print(i)
