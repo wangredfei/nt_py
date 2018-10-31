5fe41edf726e90138232650fc12b68e751df0843
xiaoAnn <289498360@qq.com>
2018-10-31 09:59:38 +0800

    MyRange_iterable

--- lxzy_19/zy1.py
+++ lxzy_19/zy1.py
@@ -15,40 +15,39 @@
 #           print(x)  # 10, 7, 4, 1
 class MyRange:
     def __init__(self, start , stop = None , step = 1):
-        l = []
+        
         if stop is None :
             stop = start
             start = 0
-        if step < 0:
-            while  start > stop:
-                l.append( start)
-                start += step
-        elif step > 0:
-            while start < stop :
-                l.append(start)
-                start += step
-        else :
-            return
-        self.date = l
+        self.start = start
+        self.stop = stop
+        self.step = step
+
+        
     def __iter__(self):
-        return MyRangeNext(self.date)
+        return MyRangeNext(self.start, self.stop, self.step)
 class MyRangeNext():
-    def __init__ (self,date):
-        self.date = date
-        self.index = 0
+    def __init__ (self,start, stop, step):
+        self.start = start
+        self.stop = stop
+        self.step = step
+
     def __next__(self):
-
-        if self.index >= len(self.date):
-            raise StopIteration
-            '''
-            不报错的原因是, 其实报错
-            迭代器协议是指对象能够使用next函数获取下一项数据,
-            在没有下一项数据时出发一个StopIteration异常来终止迭代的约定
-            '''
-        r = self.date[self.index]
-        self.index += 1
-        return  r
-
+        if self.step < 0:
+            while  self.start > self.stop:
+                r = self.start
+                self.start += self.step
+                return  r
+        elif self.step > 0:
+            while self.start < self.stop :
+                r = self.start
+                self.start += self.step
+                return  r
+        # 包含了步长为0的时候
+        raise StopIteration 
+            # 不报错的原因是, 其实报错
+            # 迭代器协议是指对象能够使用next函数获取下一项数据,
+            # 在没有下一项数据时出发一个StopIteration异常来终止迭代的约定
 L = list(MyRange(5))
 print(L)  # [0, 1, 2, 3, 4]
 print(sum(MyRange(1, 101)))  # 5050
