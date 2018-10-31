3982f555742e5ea8cd89c69578c55d0fe14e0189
xiaoAnn <289498360@qq.com>
2018-10-31 09:22:06 +0800

    myrangeclass iter

--- lxzy_19/zy1.py
+++ lxzy_19/zy1.py
@@ -15,9 +15,39 @@
 #           print(x)  # 10, 7, 4, 1
 class MyRange:
     def __init__(self, start , stop = None , step = 1):
-        if 
+        l = []
+        if stop is None :
+            stop = start
+            start = 0
+        if step < 0:
+            while  start > stop:
+                l.append( start)
+                start += step
+        elif step > 0:
+            while start < stop :
+                l.append(start)
+                start += step
+        else :
+            return
+        self.date = l
     def __iter__(self):
-        ...
+        return MyRangeNext(self.date)
+class MyRangeNext():
+    def __init__ (self,date):
+        self.date = date
+        self.index = 0
+    def __next__(self):
+
+        if self.index >= len(self.date):
+            raise StopIteration
+            '''
+            不报错的原因是, 其实报错
+            迭代器协议是指对象能够使用next函数获取下一项数据,
+            在没有下一项数据时出发一个StopIteration异常来终止迭代的约定
+            '''
+        r = self.date[self.index]
+        self.index += 1
+        return  r
 
 L = list(MyRange(5))
 print(L)  # [0, 1, 2, 3, 4]
